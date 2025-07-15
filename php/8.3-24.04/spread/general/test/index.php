84ec57175b4168dc43c11505edc5326adf45cda1
Alex Santisteban <alex.santisteban@canonical.com>
2025-07-15 19:11:03 +0200

    test: update tests

--- php/8.3-24.04/spread/general/test/index.php
+++ php/8.3-24.04/spread/general/test/index.php
@@ -1,3 +1,24 @@
 <?php
-    echo "hello, world!";
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// Sanitize path for routing
+switch ($path) {
+    case '/':
+    case '/index.php':
+        echo 'hello, world';
+        break;
+
+    case '/api':
+        include 'pages/api.php';
+        break;
+
+    case '/form':
+        include 'pages/form.php';
+        break;
+
+    default:
+        http_response_code(404);
+        echo 'Not Found';
+        break;
+}
 ?>
